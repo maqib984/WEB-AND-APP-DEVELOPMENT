cbd5e655878abf7d2918b9f15d92ce50a2b5fd6e
Muhammad Aqib <aqibk3037@gmail.com>
2023-08-11 11:42:30 +0500

    Javascript

---
+++ JavaScript/app.js
@@ -0,0 +1,27 @@
+        //   ========ALERT========
+// alert ("Error! Please Enter a Valid Password")
+// alert ("Welcome to JS Land...\nHappy Coding!")
+// alert ("Happy Coding!\nPrevent this page from creating additional dialogs.")
+// alert ("Hello... I can run JS through my web browser's console")
+
+    //    ========VARIABLES FOR STRINGS========
+// var userName
+// var myName = "Muhammad Aqib"
+// var message;
+// message = "Hello World";
+// alert (message);
+// var studentName = "Muhammad aqib";
+// var studentAge = "23 years old";
+// var studentQualification = "Intermediate";
+// alert (studentName)
+// alert (studentAge)
+// alert (studentQualification)
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert (pizza)
+// var email = "My email Address is aqibk3037@gmail.com"
+// alert (email)
+// var book = "A smarter way to learn JavaScript"
+// alert (book )
+// document.write ("Yah! I can write HTML content through JavaScript")
+var design ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert (design)
