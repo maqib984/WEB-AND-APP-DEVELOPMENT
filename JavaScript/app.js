a1a4c4ca4db6da969f4db928b8add1930d6ba9d6
Muhammad Aqib <aqibk3037@gmail.com>
2023-08-11 21:15:51 +0500

    javascript Chapter3

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,9 +1,11 @@
-        //   ========ALERT========
+        //  ========CHAPTER 01====
+    //   ========ALERT========
 // alert ("Error! Please Enter a Valid Password")
 // alert ("Welcome to JS Land...\nHappy Coding!")
 // alert ("Happy Coding!\nPrevent this page from creating additional dialogs.")
 // alert ("Hello... I can run JS through my web browser's console")
 
+            //   ========CHAPTER 02====
     //    ========VARIABLES FOR STRINGS========
 // var userName
 // var myName = "Muhammad Aqib"
@@ -18,10 +20,34 @@
 // alert (studentQualification)
 // var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
 // alert (pizza)
-// var email = "My email Address is aqibk3037@gmail.com"
-// alert (email)
+// var email = "aqibk3037@gmail.com"
+// alert ("My email address is " + email)
 // var book = "A smarter way to learn JavaScript"
-// alert (book )
+// alert ("I am trying to learn a book " + book )
 // document.write ("Yah! I can write HTML content through JavaScript")
-var design ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
-alert (design)
+// var design ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert (design)
+
+        //       ========CHAPTER 03=========
+        //  ========VARIABLES FOR NUMBERS==========
+// var age = "I'm 23 years old"
+// alert (age)
+// var visited = "1"
+// alert ("Ypu have visited this site " + visited + " time")
+// var visited = "2"
+// alert ("Ypu have visited this site " + visited + " time")
+// var visited = "3"
+// alert ("Ypu have visited this site " + visited + " time")
+// var visited = "4"
+// alert ("Ypu have visited this site " + visited + " time")
+// var birthYear = 1999
+// document.write ("My birth year is " + birthYear + "<br />Data type of my declared virable is number")
+// var visiterName = "Aqib"
+// var productTitle = "T-Shirts"
+// var quantity = 5
+// document.write (visiterName + " ordered " + quantity + " " + productTitle + " on My Store")
+// var a,b,c = 
+
+
+
+
