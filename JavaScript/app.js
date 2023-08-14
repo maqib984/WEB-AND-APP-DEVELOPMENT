fe07d937fc56fb28fbfadcdf5b6ee091a7571fed
Muhammad Aqib <aqibk3037@gmail.com>
2023-08-14 20:39:55 +0500

    added my portfolio

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -306,5 +306,46 @@ document.write("<h4>ANS OF QUESTION NO 04</h4>")
 // }
 document.write("----------------------------------------------------------- " + "<br />")
 
+document.write("<h4>ANS OF QUESTION NO 05</h4>")
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 82){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (++c === 13){
+// alert("condition 1 is true");
+// }
+// if (++c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// // }
+// if(c+2 === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// var equals = totalCost - 2000
+// if (equals){
+// alert("The cost equals");
+// }
+// var myName = prompt("My Name is", "Aqib")
 
+// if (myName === "Aqib"){
+//         alert("True");
+//         }
+// if (myName !== "Aqib"){
+//     alert("False");
+// }
+// var cat = 50
+// var car = 49
+// if(car < cat){
+//         alert("car is smaller than cat");
+//  }
 
