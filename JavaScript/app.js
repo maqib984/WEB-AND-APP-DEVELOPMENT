194bbc0f48ee34443a061eb1c0083e7a87a30937
Muhammad Aqib <aqibk3037@gmail.com>
2023-08-20 23:03:49 +0500

    assignment add

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -66,169 +66,169 @@ document.write("<h1>CHAPTER 05</h1>")
 document.write("<h1>MATH EXPRESSION</h1>")
 
 document.write("<h1>Ans Of Question No 01</h1>")
-sum = 3 + 5
-document.write("The Sum of 3 and 5 is " + sum + "<br />")
+// sum = 3 + 5
+// document.write("The Sum of 3 and 5 is " + sum + "<br />")
 
 document.write("<h1>Ans Of Question No 02</h1>")
-var multiple = 3 * 5
-document.write("The Multiple of 3 and 5 is " + multiple + "<br />")
-var subtract = 3 - 5
-document.write("The Subtract of 3 and 5 is " + subtract + "<br />")
-var division = 3 / 5
-document.write("The Division of 3 and 5 is " + division + "<br />")
-var modulus = 3 % 5
-document.write("The Mudulus of 3 and 5 is " + modulus + "<br />")
+// var multiple = 3 * 5
+// document.write("The Multiple of 3 and 5 is " + multiple + "<br />")
+// var subtract = 3 - 5
+// document.write("The Subtract of 3 and 5 is " + subtract + "<br />")
+// var division = 3 / 5
+// document.write("The Division of 3 and 5 is " + division + "<br />")
+// var modulus = 3 % 5
+// document.write("The Mudulus of 3 and 5 is " + modulus + "<br />")
 
 document.write("<h1>Ans Of Question No 03</h1>")
-var declare
-document.write("Value after variable declaration is " + declare + "<br />")
-var num = 5
-document.write("Initial Value: " + num + "<br />")
-var newNum = ++num
-document.write("Value after increment is: " + newNum + "<br />")
-var add = newNum + 7
-document.write("Value after addition is: " + add + "<br />")
-var decrement = --add
-document.write("Value after decrement is: " + decrement + "<br />")
-var remainder = decrement / 3
-document.write("The remainder is: " + remainder + "<br />")
+// var declare
+// document.write("Value after variable declaration is " + declare + "<br />")
+// var num = 5
+// document.write("Initial Value: " + num + "<br />")
+// var newNum = ++num
+// document.write("Value after increment is: " + newNum + "<br />")
+// var add = newNum + 7
+// document.write("Value after addition is: " + add + "<br />")
+// var decrement = --add
+// document.write("Value after decrement is: " + decrement + "<br />")
+// var remainder = decrement / 3
+// document.write("The remainder is: " + remainder + "<br />")
 
 document.write("<h1>Ticket</h1>")
-var costOfTicket = 600
-var buyer = 5
-var totalCost = costOfTicket * buyer
-document.write("Total cost to buy " + buyer + " tickets to a movie is " + totalCost + "<br />")
+// var costOfTicket = 600
+// var buyer = 5
+// var totalCost = costOfTicket * buyer
+// document.write("Total cost to buy " + buyer + " tickets to a movie is " + totalCost + "<br />")
 
 document.write("<h1>Table</h1>")
-var table = 4;
-var num1 = 1;
-var num2 = 2;
-var num3 = 3;
-var num4 = 4;
-var num5 = 5;
-var num6 = 6;
-var num7 = 7;
-var num8 = 8;
-var num9 = 9;
-var num10 = 10;
-var multi1 = table * num1;
-var multi2 = table * num2;
-var multi3 = table * num3;
-var multi4 = table * num4;
-var multi5 = table * num5;
-var multi6 = table * num6;
-var multi7 = table * num7;
-var multi8 = table * num8;
-var multi9 = table * num9;
-var multi10 = table * num10;
-document.write("Table Of " + table + "<br />");
-document.write(table + " x " + num1 + " = " + multi1 + "<br />");
-document.write(table + " x " + num2 + " = " + multi2 + "<br />");
-document.write(table + " x " + num3 + " = " + multi3 + "<br />");
-document.write(table + " x " + num4 + " = " + multi4 + "<br />");
-document.write(table + " x " + num5 + " = " + multi5 + "<br />");
-document.write(table + " x " + num6 + " = " + multi6 + "<br />");
-document.write(table + " x " + num7 + " = " + multi7 + "<br />");
-document.write(table + " x " + num8 + " = " + multi8 + "<br />");
-document.write(table + " x " + num9 + " = " + multi9 + "<br />");
-document.write(table + " x " + num10 + " = " + multi10 + "<br />");
+// var table = 4;
+// var num1 = 1;
+// var num2 = 2;
+// var num3 = 3;
+// var num4 = 4;
+// var num5 = 5;
+// var num6 = 6;
+// var num7 = 7;
+// var num8 = 8;
+// var num9 = 9;
+// var num10 = 10;
+// var multi1 = table * num1;
+// var multi2 = table * num2;
+// var multi3 = table * num3;
+// var multi4 = table * num4;
+// var multi5 = table * num5;
+// var multi6 = table * num6;
+// var multi7 = table * num7;
+// var multi8 = table * num8;
+// var multi9 = table * num9;
+// var multi10 = table * num10;
+// document.write("Table Of " + table + "<br />");
+// document.write(table + " x " + num1 + " = " + multi1 + "<br />");
+// document.write(table + " x " + num2 + " = " + multi2 + "<br />");
+// document.write(table + " x " + num3 + " = " + multi3 + "<br />");
+// document.write(table + " x " + num4 + " = " + multi4 + "<br />");
+// document.write(table + " x " + num5 + " = " + multi5 + "<br />");
+// document.write(table + " x " + num6 + " = " + multi6 + "<br />");
+// document.write(table + " x " + num7 + " = " + multi7 + "<br />");
+// document.write(table + " x " + num8 + " = " + multi8 + "<br />");
+// document.write(table + " x " + num9 + " = " + multi9 + "<br />");
+// document.write(table + " x " + num10 + " = " + multi10 + "<br />");
 
-var priceOfItem1 = 650
-var priceOfItem2 = 100
-var orderOfItem1 = 3
-var orderOfItem2 = 7
-var charges = 100
-var totalCost = priceOfItem1 * orderOfItem1 + priceOfItem2 * orderOfItem2 + charges
-document.write("<h1>Shopping Cart</h1><br / >")
-document.write("Price of item 1 is " + priceOfItem1 + "<br />")
-document.write("Order of item 1 is " + orderOfItem1 + "<br />")
-document.write("Price of item 2 is " + priceOfItem2 + "<br />")
-document.write("Order of item 2 is " + orderOfItem2 + "<br />")
-document.write("Service Charges " + charges + "<br />")
-document.write("<br />")
-document.write("Total cost of Orders is " + totalCost)
+// var priceOfItem1 = 650
+// var priceOfItem2 = 100
+// var orderOfItem1 = 3
+// var orderOfItem2 = 7
+// var charges = 100
+// var totalCost = priceOfItem1 * orderOfItem1 + priceOfItem2 * orderOfItem2 + charges
+// document.write("<h1>Shopping Cart</h1><br / >")
+// document.write("Price of item 1 is " + priceOfItem1 + "<br />")
+// document.write("Order of item 1 is " + orderOfItem1 + "<br />")
+// document.write("Price of item 2 is " + priceOfItem2 + "<br />")
+// document.write("Order of item 2 is " + orderOfItem2 + "<br />")
+// document.write("Service Charges " + charges + "<br />")
+// document.write("<br />")
+// document.write("Total cost of Orders is " + totalCost)
 
-var totalMarks = 980
-var marksObtained = 804
-var percentage = marksObtained / totalMarks * 100
-document.write("<h1>Mark Sheet</h1> <br />")
-document.write("Total Marks: " + totalMarks + "<br />")
-document.write("Marks Obtained: " + marksObtained + "<br />")
-document.write("Percentage: " + percentage + "%")
+// var totalMarks = 980
+// var marksObtained = 804
+// var percentage = marksObtained / totalMarks * 100
+// document.write("<h1>Mark Sheet</h1> <br />")
+// document.write("Total Marks: " + totalMarks + "<br />")
+// document.write("Marks Obtained: " + marksObtained + "<br />")
+// document.write("Percentage: " + percentage + "%")
 
-var dollar = 10
-var riyal = 25
-var dollarPrice = 104.80
-var riyalPrice = 28
-var totalCurrency = dollarPrice * dollar + riyalPrice * riyal
-document.write("<h1>Currency in PKR</h1> <br />")
-document.write("Total Currency in PKR: " + totalCurrency)
+// var dollar = 10
+// var riyal = 25
+// var dollarPrice = 104.80
+// var riyalPrice = 28
+// var totalCurrency = dollarPrice * dollar + riyalPrice * riyal
+// document.write("<h1>Currency in PKR</h1> <br />")
+// document.write("Total Currency in PKR: " + totalCurrency)
 
-// var num = (2 + 5) * 10 / 2
-// alert(num)
+// // var num = (2 + 5) * 10 / 2
+// // alert(num)
 
-var currentYear = 2023
-var birthYear = 1999
-var myAge = currentYear - birthYear
-document.write("<h1>Age Calculator</h1> <br />")
-document.write("Current Year: " + currentYear + "<br />")
-document.write("Birth Year: " + birthYear + "<br />")
-document.write("My Age is: " + myAge)
+// var currentYear = 2023
+// var birthYear = 1999
+// var myAge = currentYear - birthYear
+// document.write("<h1>Age Calculator</h1> <br />")
+// document.write("Current Year: " + currentYear + "<br />")
+// document.write("Birth Year: " + birthYear + "<br />")
+// document.write("My Age is: " + myAge)
 
-var snack = "Lays"
-var currentAge = 15
-var estimateAge = 65
-var snackPerDay = 3
-var totalSnack = (estimateAge - currentAge) * snackPerDay
-document.write("<h1>The Lifeime Supply Calculator</h1> <br />")
-document.write("Favourite Snack: " + snack + "<br />")
-document.write("Current Age: " + currentAge + "<br />")
-document.write("Estimated Maximun Age: " + estimateAge + "<br />")
-document.write("Amount of Snack Per Day: " + snackPerDay + "<br />")
-document.write("You will need " + totalSnack + " " + snack + " to last you until the ripe old age of " + estimateAge)
+// var snack = "Lays"
+// var currentAge = 15
+// var estimateAge = 65
+// var snackPerDay = 3
+// var totalSnack = (estimateAge - currentAge) * snackPerDay
+// document.write("<h1>The Lifeime Supply Calculator</h1> <br />")
+// document.write("Favourite Snack: " + snack + "<br />")
+// document.write("Current Age: " + currentAge + "<br />")
+// document.write("Estimated Maximun Age: " + estimateAge + "<br />")
+// document.write("Amount of Snack Per Day: " + snackPerDay + "<br />")
+// document.write("You will need " + totalSnack + " " + snack + " to last you until the ripe old age of " + estimateAge)
 
 document.write("<h1>CHAPTER 06</h1>")
 document.write("<h1>MATH EXPRESSION</h1>")
 
 document.write("<h4>ANS OF QUESTION NO 01</h4>")
-var a = 10
-document.write("The value of a is " + a + "<br />")
-document.write("------------------------------ " + "<br />")
+// var a = 10
+// document.write("The value of a is " + a + "<br />")
+// document.write("------------------------------ " + "<br />")
 
-var result = (++a)
-document.write("The value of ++a is " + result + "<br />")
-document.write("Now value of a is " + a + "<br />")
-document.write("------------------------------ " + "<br />")
+// var result = (++a)
+// document.write("The value of ++a is " + result + "<br />")
+// document.write("Now value of a is " + a + "<br />")
+// document.write("------------------------------ " + "<br />")
 
-var result = (a++)
-document.write("The value of a++ is " + result + "<br />")
-document.write("Now value of a is " + a + "<br />")
-document.write("------------------------------ " + "<br />")
+// var result = (a++)
+// document.write("The value of a++ is " + result + "<br />")
+// document.write("Now value of a is " + a + "<br />")
+// document.write("------------------------------ " + "<br />")
 
-var result = (--a)
-document.write("The value of --a is " + result + "<br />")
-document.write("Now value of a is " + a + "<br />")
-document.write("------------------------------ " + "<br />")
+// var result = (--a)
+// document.write("The value of --a is " + result + "<br />")
+// document.write("Now value of a is " + a + "<br />")
+// document.write("------------------------------ " + "<br />")
 
-var result = (a--)
-document.write("The value of a-- is " + result + "<br />")
-document.write("Now value of a is " + a + "<br />")
-document.write("------------------------------ " + "<br />")
+// var result = (a--)
+// document.write("The value of a-- is " + result + "<br />")
+// document.write("Now value of a is " + a + "<br />")
+// document.write("------------------------------ " + "<br />")
 
 document.write("<h4>ANS OF QUESTION NO 02</h4>")
-var a = 2, b = 1
-var result = --a - --b + ++b + b--;
-document.write("The value of a is " + a + "<br />")
-document.write("The value of b is " + b + "<br />")
-var result = 1
-document.write("--a: " + result + "<br />")
-var result = 1 - 0
-document.write("--a - --b: " + result + "<br />")
-var result = 1 - 0 + 1
-document.write("--a - --b + ++b: " + result + "<br />")
-var result = 1 - 0 + 1 + 1
-document.write("--a - --b + ++b + b-- : " + result + "<br />")
-document.write("------------------------------ " + "<br />")
+// var a = 2, b = 1
+// var result = --a - --b + ++b + b--;
+// document.write("The value of a is " + a + "<br />")
+// document.write("The value of b is " + b + "<br />")
+// var result = 1
+// document.write("--a: " + result + "<br />")
+// var result = 1 - 0
+// document.write("--a - --b: " + result + "<br />")
+// var result = 1 - 0 + 1
+// document.write("--a - --b + ++b: " + result + "<br />")
+// var result = 1 - 0 + 1 + 1
+// document.write("--a - --b + ++b + b-- : " + result + "<br />")
+// document.write("------------------------------ " + "<br />")
 
 document.write("<h4>ANS OF QUESTION NO 03</h4>")
 document.write("<h2>Comment</h2>")
@@ -238,23 +238,23 @@ document.write("------------------------------ " + "<br />")
 
 document.write("<h4>ANS OF QUESTION NO 06</h4>")
 document.write("<h4>Subject Total Marks Obtained Marks Percentage</h4>")
-var marks = 100
-var totalMarks = marks * 3
-var subject1 = "Math"
-var obtaineSubject1 = 54
-var percentage1 = obtaineSubject1 / marks * 100
-var subject2 = "English"
-var obtaineSubject2 = 54
-var percentage2 = obtaineSubject2 / marks * 100
-var subject3 = "Urdu"
-var obtaineSubject3 = 48
-var percentage3 = obtaineSubject3 * marks / 100
-var totalobatined = obtaineSubject1 + obtaineSubject2 + obtaineSubject3
-var totalpercentage = totalobatined / totalMarks * 100
-document.write("Math " + marks + "   " + obtaineSubject1 + " " + percentage1 + "% <br />")
-document.write("English " + marks + "   " + obtaineSubject2 + " " + percentage2 + "% <br />")
-document.write("Urdu " + marks + "   " + obtaineSubject3 + " " + percentage3 + "%")
-document.write("<h5>Total Marks " + totalMarks + " Obtained Marks " + totalobatined + " Total Percentage " + totalpercentage + "%" + "</h5>")
+// var marks = 100
+// var totalMarks = marks * 3
+// var subject1 = "Math"
+// var obtaineSubject1 = 54
+// var percentage1 = obtaineSubject1 / marks * 100
+// var subject2 = "English"
+// var obtaineSubject2 = 54
+// var percentage2 = obtaineSubject2 / marks * 100
+// var subject3 = "Urdu"
+// var obtaineSubject3 = 48
+// var percentage3 = obtaineSubject3 * marks / 100
+// var totalobatined = obtaineSubject1 + obtaineSubject2 + obtaineSubject3
+// var totalpercentage = totalobatined / totalMarks * 100
+// document.write("Math " + marks + "   " + obtaineSubject1 + " " + percentage1 + "% <br />")
+// document.write("English " + marks + "   " + obtaineSubject2 + " " + percentage2 + "% <br />")
+// document.write("Urdu " + marks + "   " + obtaineSubject3 + " " + percentage3 + "%")
+// document.write("<h5>Total Marks " + totalMarks + " Obtained Marks " + totalobatined + " Total Percentage " + totalpercentage + "%" + "</h5>")
 document.write("----------------------------------------------------------- " + "<br />")
 
 document.write("<h1>CHAPTER 07</h1>")
@@ -348,4 +348,80 @@ document.write("<h4>ANS OF QUESTION NO 05</h4>")
 // if(car < cat){
 //         alert("car is smaller than cat");
 //  }
+
+document.write("<h1>CHAPTER 14 - 16 </h1>")
+document.write("<h1>ARRAYS</h1>")
+document.write("<h4>ANS OF QUESTION NO 03 04 05 06</h4>")
+// var names = ["Muhammad", "Aqib", "khan"]
+// var num = [9, 8 , 4]
+// var bool = [null , false , null]
+// var mixed = ["aqib" , 984 , null]
+// var education = ["SSC", "HSC" , "BSC", "BS", "BCOM", 'MS', "M.Phil", "PhD"]
+// document.write ("<h3>Qulaifications:</h3>")
+// document.write ("1)" + education [0] + "<br/>")
+// document.write ("2)" + education [1] + "<br/>")
+// document.write ("3)" + education [2] + "<br/>")
+// document.write ("4)" + education [3] + "<br/>")
+// document.write ("5)" + education [4] + "<br/>")
+// document.write ("6)" + education [5] + "<br/>")
+// document.write ("7)" + education [6] + "<br/>")
+// document.write ("8)" + education [7] + "<br/>")
+
+document.write ("<h4>ANS OF QUESTION NO 08</h4>")
+
+// var students = ["Aqib", "Hassan", "Arsalan"]
+// var score = [420, 300, 480]
+// var totalScore = (500)
+// document.write ("Score of " + students[0] + " is " + score[0] + " .Percentage: " + score[0]/totalScore*100 + "% <br />")
+// document.write ("Score of " + students[1] + " is " + score[1] + " .Percentage: " + score[1]/totalScore*100 + "% <br />")
+// document.write ("Score of " + students[2] + " is " + score[2] + " .Percentage: " + score[2]/totalScore*100 + "% <br />")
+
+document.write ("<h4>ANS OF QUESTION NO 09</h4>")
+// var colors = ["blue" , "black" , "red"]
+// // document.write (colors)
+// var addColor = ("orange")
+// colors.unshift (addColor)
+// // document.write (colors)
+// var addColorEnd = ("yellow")
+// colors.push (addColorEnd)
+// // document.write (colors)
+// colors.unshift ("grey", "purple")
+// // document.write (colors)
+// colors.shift ()
+// // document.write (colors)
+// colors.pop ()
+// // document.write (colors)
+// var addColorMiddle = ("white")
+// colors.splice (2,0 , addColorMiddle)
+// // document.write (colors)
+// var deleteColorAfterWhite;
+// colors.splice (3,)
+// document.write (colors)
+
+document.write ("<h4>ANS OF QUESTION NO 11</h4>")
+
+// var cities = ["karachi", "lahore", "Peshawar" , "islamabad"]
+// document.write ("<h4>Cities:</h4>" + cities + "<br />")
+// var selectedCities = cities.slice (0,2)
+// document.write ("<h4>Cities Selected:</h4>" + selectedCities)
+
+document.write ("<h4>ANS OF QUESTION NO 12</h4>")
+
+// var str = ["This", "is", "my", "cat"]
+// str.join (str)
+// document.write (str)
+
+document.write ("<h4>ANS OF QUESTION NO 13</h4>")
+
+// var device = ["keyboard", "mouse", "printer", "Monitor"]
+// document.write ("<h4>Devices:</h4>" + device + "<br />")
+
+// var keyboardOut = device.splice (0,1)
+// document.write ("<h4>Out:</h4>" + keyboardOut + "<br />")
+// var mouseOut = device.splice (0,1)
+// document.write ("<h4>Out:</h4>" + mouseOut + "<br />")
+// var printerOut = device.splice (0,1)
+// document.write ("<h4>Out:</h4>" + printerOut + "<br />")
+// var monitorOut = device.splice (0,1)
+// document.write ("<h4>Out:</h4>" + monitorOut + "<br />")
 
